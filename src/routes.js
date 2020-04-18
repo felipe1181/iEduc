1c50695ca2a0227d96be720a7fcb50b1df87cc12
felipe1181 <felipe1181jua@gmail.com>
2020-04-17 21:13:30 -0300

    arquivo de routes antigo deletado

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-unresolved */
-const express = require('express');
-const UserController = require('./app/user/UserControllerApp');
-const routes = express.Router();
-
-
-routes.post('/users', UserController.store);
-
-module.exports = routes;
